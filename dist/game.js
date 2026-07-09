c353f56574113e4a9092181fcfc5b879c6ad72a1
Sandro <Sandro.bonomo@gmx.de>
2026-07-09 17:14:00 +0200

    fix: playground with grid layout, feat: codeImages array, getImages function to get as many images from image array as pairAmount calculated

--- dist/game.js
+++ dist/game.js
@@ -16,4 +16,76 @@ const playGround = document.querySelector("#playGround");
 if (gameBody) {
     gameBody.classList.add(`theme-${gameSettings.theme}`);
 }
+const codeCardBack = "../../src/assets/img/game_page/code-theme/main/cards/basic_card_back.png";
+const codeImages = [
+    "../../src/assets/img/game_page/code-theme/html_card.png",
+    "../../src/assets/img/game_page/code-theme/css_card.png",
+    "../../src/assets/img/game_page/code-theme/typescript_card.png",
+    "../../src/assets/img/game_page/code-theme/javascript_card.png",
+    "../../src/assets/img/game_page/code-theme/angular_card.png",
+    "../../src/assets/img/game_page/code-theme/bootstrap_card.png",
+    "../../src/assets/img/game_page/code-theme/console_card.png",
+    "../../src/assets/img/game_page/code-theme/django_card.png",
+    "../../src/assets/img/game_page/code-theme/git_card.png",
+    "../../src/assets/img/game_page/code-theme/git_hub_card.png",
+    "../../src/assets/img/game_page/code-theme/firebase_card.png",
+    "../../src/assets/img/game_page/code-theme/node_card.png",
+    "../../src/assets/img/game_page/code-theme/python_card.png",
+    "../../src/assets/img/game_page/code-theme/react_card.png",
+    "../../src/assets/img/game_page/code-theme/sass_card.png",
+    "../../src/assets/img/game_page/code-theme/vs_code_card.png",
+    "../../src/assets/img/game_page/code-theme/vueJS_card.png",
+    "../../src/assets/img/game_page/code-theme/sql_card.png",
+];
+const themeImages = {
+    code: codeImages,
+    gaming: gamingImages,
+    da: daImages
+};
+const currentThemeImages = themeImages[gameSettings.theme];
+function getImages() {
+    const pairAmount = Number(gameSettings.cards) / 2;
+    const selectedImages = [];
+    for (let i = 0; i < pairAmount; i++) {
+        selectedImages.push(currentThemeImages[i]);
+    }
+    return selectedImages;
+}
+function createCard() {
+    return `
+        <div class="card">
+            
+            ...
+        </div>
+    `;
+}
+function placeCards() {
+    switch (gameSettings.cards) {
+        case "24":
+            createCard(card);
+            break;
+        case "36":
+            break;
+        default:
+            break;
+    }
+}
+function shuffleCards() {
+}
+function determinePlayer() {
+}
+function handleCardClick(card) {
+}
+function turnCard(card) {
+}
+function hideCards() {
+}
+function compareCards() {
+}
+function handlePair() {
+}
+function changePlayer() {
+}
+function checkGameOver() {
+}
 export {};
--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,7 @@
+type Theme = "code" | "gaming" | "da";
+
 type GameSettings = {
-    theme: string;
+    theme: Theme;
     player: string;
     cards: string;
 };
@@ -30,10 +32,76 @@ if (gameBody) {
 }
 
 
+//code-theme images:
+const codeCardBack = "../../src/assets/img/game_page/code-theme/main/cards/basic_card_back.png" 
+
+const codeImages = [
+    "../../src/assets/img/game_page/code-theme/html_card.png",
+    "../../src/assets/img/game_page/code-theme/css_card.png",
+    "../../src/assets/img/game_page/code-theme/typescript_card.png",
+    "../../src/assets/img/game_page/code-theme/javascript_card.png",
+    "../../src/assets/img/game_page/code-theme/angular_card.png",
+    "../../src/assets/img/game_page/code-theme/bootstrap_card.png",
+    "../../src/assets/img/game_page/code-theme/console_card.png",
+    "../../src/assets/img/game_page/code-theme/django_card.png",
+    "../../src/assets/img/game_page/code-theme/git_card.png",
+    "../../src/assets/img/game_page/code-theme/git_hub_card.png",
+    "../../src/assets/img/game_page/code-theme/firebase_card.png",
+    "../../src/assets/img/game_page/code-theme/node_card.png",
+    "../../src/assets/img/game_page/code-theme/python_card.png",
+    "../../src/assets/img/game_page/code-theme/react_card.png",
+    "../../src/assets/img/game_page/code-theme/sass_card.png",
+    "../../src/assets/img/game_page/code-theme/vs_code_card.png",
+    "../../src/assets/img/game_page/code-theme/vueJS_card.png",
+    "../../src/assets/img/game_page/code-theme/sql_card.png",
+];
+
+
+const themeImages: Record<Theme, string[]> = {
+    code: codeImages,
+    gaming: gamingImages,
+    da: daImages
+};
+
+
 // functions needed for memory:
+
+// get the number of needed pairs for game
+const currentThemeImages = themeImages[gameSettings.theme];
+function getImages(): string[] {
+    const pairAmount = Number(gameSettings.cards)/2;
+    const selectedImages:string[] = [];
+    
+    for (let i = 0; i < pairAmount; i++) {
+        selectedImages.push(currentThemeImages[i]);
+    }
+    return selectedImages;
+}
+
+function createCard() {
+    return `
+        <div class="card">
+            
+            ...
+        </div>
+    `;
+}
+
 function placeCards() {
     //inital function to add the cards to the board
     //by using cards from GameSettings
+    switch (gameSettings.cards) {
+        case "24":
+            createCard(card);
+            break;
+        case "36":
+            
+            break;
+    
+        default:
+
+            break;
+    }
 }
 
 function shuffleCards() {
@@ -75,5 +143,6 @@ function checkGameOver() {
     //checks how many cards left
     //ends game when no cards left
 }
+
 
 export {};
