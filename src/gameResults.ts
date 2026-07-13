0fec1461620071a0ebddae63b41d7aae4a7d65f1
Sandro <Sandro.bonomo@gmx.de>
2026-07-13 17:03:08 +0200

    initalised endscreen html file. initalised gameResults ts file, feat: loading winner, scores and theme from sessional storage. feat: body bg dynamic to the theme.

---
+++ src/gameResults.ts
@@ -0,0 +1,14 @@
+const winner = JSON.parse(sessionStorage.getItem("winningPlayer:") || '""');
+const scoreB = JSON.parse(sessionStorage.getItem("blue score:") || "0");
+const scoreO= JSON.parse(sessionStorage.getItem("orange score:") || "0");
+const theme = JSON.parse(sessionStorage.getItem("theme:") || "code");
+
+
+const endscreenBody = document.querySelector<HTMLBodyElement>("#gameBody");
+
+if (endscreenBody) {
+    endscreenBody.classList.add(`theme-${theme}`);
+}
+
+
+
