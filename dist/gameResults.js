ad613469d530a45fdc6abb3c589336ed36b0b4cc
Sandro <Sandro.bonomo@gmx.de>
2026-07-13 18:39:37 +0200

    feat: load scoreBoard  and scores from sessionStorage and display them.

--- dist/gameResults.js
+++ dist/gameResults.js
@@ -3,7 +3,30 @@ const winner = JSON.parse(sessionStorage.getItem("winningPlayer:") || '""');
 const scoreB = JSON.parse(sessionStorage.getItem("blue score:") || "0");
 const scoreO = JSON.parse(sessionStorage.getItem("orange score:") || "0");
 const theme = JSON.parse(sessionStorage.getItem("theme:") || "code");
+const playerIconBlue = JSON.parse(sessionStorage.getItem("playerIconBlue:") || '""');
+const playerIconOrange = JSON.parse(sessionStorage.getItem("playerIconOrange:") || '""');
 const endscreenBody = document.querySelector("#gameBody");
+const scoreIconO = document.querySelector("#scoreIconO");
+const scoreIconB = document.querySelector("#scoreIconB");
+let endScoreDisplayB = document.querySelector("#endScoreDisplayB");
+let endScoreDisplayO = document.querySelector("#endScoreDisplayO");
 if (endscreenBody) {
     endscreenBody.classList.add(`theme-${theme}`);
 }
+function initaliseScoreBoard() {
+    if (!scoreIconB || !scoreIconO)
+        return;
+    scoreIconB.src = playerIconBlue;
+    scoreIconO.src = playerIconOrange;
+}
+function initaliseScores() {
+    if (!endScoreDisplayB || !endScoreDisplayO)
+        return;
+    endScoreDisplayB.textContent = `${scoreB}`;
+    endScoreDisplayO.textContent = `${scoreO}`;
+}
+function init() {
+    initaliseScoreBoard();
+    initaliseScores();
+}
+init();
--- src/gameResults.ts
+++ src/gameResults.ts
@@ -2,13 +2,37 @@ const winner = JSON.parse(sessionStorage.getItem("winningPlayer:") || '""');
 const scoreB = JSON.parse(sessionStorage.getItem("blue score:") || "0");
 const scoreO= JSON.parse(sessionStorage.getItem("orange score:") || "0");
 const theme = JSON.parse(sessionStorage.getItem("theme:") || "code");
-
+const playerIconBlue = JSON.parse(sessionStorage.getItem("playerIconBlue:") || '""');
+const playerIconOrange = JSON.parse(sessionStorage.getItem("playerIconOrange:") || '""');
 
 const endscreenBody = document.querySelector<HTMLBodyElement>("#gameBody");
+
+const scoreIconO = document.querySelector<HTMLImageElement>("#scoreIconO");
+const scoreIconB = document.querySelector<HTMLImageElement>("#scoreIconB");
+
+let endScoreDisplayB = document.querySelector<HTMLParagraphElement>("#endScoreDisplayB");
+let endScoreDisplayO = document.querySelector<HTMLParagraphElement>("#endScoreDisplayO");
 
 if (endscreenBody) {
     endscreenBody.classList.add(`theme-${theme}`);
 }
 
+function initaliseScoreBoard() {
+    if(!scoreIconB || !scoreIconO) return;
+    scoreIconB.src = playerIconBlue;
+    scoreIconO.src = playerIconOrange;
+    
+}
 
+function initaliseScores() {
+    if( !endScoreDisplayB || !endScoreDisplayO) return;
+    endScoreDisplayB.textContent = `${scoreB}`;
+    endScoreDisplayO.textContent = `${scoreO}`;
+}
 
+function init() {
+    initaliseScoreBoard();
+    initaliseScores();
+}
+
+init();
